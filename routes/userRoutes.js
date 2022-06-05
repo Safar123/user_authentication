e31a48a3f5892a132905486dfdb5b9994a294c66
Safar123 <azajsbd111@gmail.com>
2022-06-05 19:54:41 +0545

    Implementing crud routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,11 @@
 const express = require ('express');
 const { signUpUser } = require('../controller/authController');
+const { getAllUser, getSingleUser, updateUserSelf, deleteUser } = require('../controller/userController');
 
 const router = express.Router();
 
 router.route('/signUp').post(signUpUser);
-
+router.route('/').get(getAllUser);
+router.route('/:id').get(getSingleUser).patch(updateUserSelf).delete(deleteUser);
 
 module.exports = router;
