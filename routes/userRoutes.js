e90ed6f923bfc210b81eb160aa262685285676cc
Safar123 <azajsbd111@gmail.com>
2022-06-05 21:05:58 +0545

    login route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,5 @@
 const express = require ('express');
-const { signUpUser } = require('../controller/authController');
+const { signUpUser, logInUser } = require('../controller/authController');
 const { getAllUser, getSingleUser, updateUserSelf, deleteUser } = require('../controller/userController');
 
 const router = express.Router();
@@ -7,5 +7,6 @@ const router = express.Router();
 router.route('/signUp').post(signUpUser);
 router.route('/').get(getAllUser);
 router.route('/:id').get(getSingleUser).patch(updateUserSelf).delete(deleteUser);
+router.route('/login').post(logInUser);
 
 module.exports = router;
