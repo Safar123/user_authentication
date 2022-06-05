a81c75e135509b27eda97615e6b7e545d08952e7
Safar123 <azajsbd111@gmail.com>
2022-06-05 12:01:59 +0545

    body parser and route included

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const express = require('express');
 
 const app = express();
+app.use(express.json()); //! body parse to parse user input into Json
+
+
+//!requiring routes
+const userRoutes = require('./routes/userRoutes');
+
+
+//!using routes middelware
+app.use('/api/v1/users', userRoutes)
+
 
 
 
