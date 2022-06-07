3815e4f9e92917a989228949d0c1ce17efe2541b
Safar123 <azajsbd111@gmail.com>
2022-06-07 13:05:25 +0545

    implemented error middleware

--- app.js
+++ app.js
@@ -3,10 +3,14 @@ const express = require("express");
 const app = express();
 app.use(express.json()); //! body parse to parse user input into Json
 
+//!middelware 
+const errorHandler = require('./controller/errorController');
+
 //!requiring routes
 const userRoutes = require("./routes/userRoutes");
 
 //!using routes middelware
 app.use("/api/v1/users", userRoutes);
+app.use(errorHandler)
 
 module.exports = app;
