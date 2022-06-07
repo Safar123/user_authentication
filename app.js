75402636739c74006748c58a6fdcdbc4d62ca083
Safar123 <azajsbd111@gmail.com>
2022-06-07 08:58:56 +0545

    better comment

--- app.js
+++ app.js
@@ -1,17 +1,12 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 app.use(express.json()); //! body parse to parse user input into Json
 
-
 //!requiring routes
-const userRoutes = require('./routes/userRoutes');
-
+const userRoutes = require("./routes/userRoutes");
 
 //!using routes middelware
-app.use('/api/v1/users', userRoutes)
-
-
-
+app.use("/api/v1/users", userRoutes);
 
 module.exports = app;
