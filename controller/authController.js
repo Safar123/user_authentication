c57bccfb6df6f3f54312abb1903890197f3fe7ab
Safar123 <azajsbd111@gmail.com>
2022-06-07 08:57:41 +0545

    login route and function

--- controller/authController.js
+++ controller/authController.js
@@ -1,60 +1,56 @@
-const User = require('../model/userModel');
-exports.signUpUser = async (req, res)=>{
-
-    if (req.body.password.match(/^[a-zA-Z][0-9]$/ )){
+const User = require("../model/userModel");
+exports.signUpUser = async (req, res) => {
+    if (req.body.password.match(/^[a-zA-Z][0-9]$/)) {
         return res.status(400).json({
-            success:false, 
-            message:'Password must contain both number and alphabet'
-        })
-     }
+            success: false,
+            message: "Password must contain both number and alphabet",
+        });
+    }
 
     const newUser = await User.create({
-        username:req.body.username,
-        email:req.body.email,
-        password:req.body.password,
-        confirmPassword:req.body.confirmPassword,
-        userImage:req.body.userImage
-    })
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password,
+        confirmPassword: req.body.confirmPassword,
+        userImage: req.body.userImage,
+    });
 
-    
- 
     res.status(201).json({
-        success:'true',
-        data:{
-            user:newUser
-        }
-    })
-}
+        success: "true",
+        data: {
+            user: newUser,
+        },
+    });
+};
 
 //!Login function
 
-exports.logInUser = async(req, res)=>{
-//!first to provide login function we need email and password from request body
-const {email, password}= req.body;
-//!check we have both emil and password
-if(!email || !password){
-    return res.status(400).json({
-        success:false,
-        message:'Both email and Password is required to login'
-    })
-}
-//!then we need to check if email is registered or not
-const user = await User.findOne({email}).select('+password');
-//!as our saved pwd is encrypted we need to compare it using same algorithm, we need to define compare functionality in user model
+exports.logInUser = async (req, res) => {
+    //!first to provide login function we need email and password from request body
+    const { email, password } = req.body;
 
-if(!user || ! await user.checkPwdEncryption(password, user.password)){
-    return res.status(400).json({
-        success:'fail',
-        message:' Either email or password is wrong'
-    })
-}
+    //!check we have both emil and password
+    if (!email || !password) {
+        return res.status(400).json({
+            success: false,
+            message: "Both email and Password is required to login",
+        });
+    }
+    //!then we need to check if email is registered or not
+    const user = await User.findOne({ email }).select("+password");
 
-if(user){
-    res.status(200).json({
-        success:true,
-        message:'User login successful'
-    })
-}
+    //!function to compre user input pwd with encrypted password in database
+    if (!user || !(await user.checkPwdEncryption(password, user.password))) {
+        return res.status(400).json({
+            success: "fail",
+            message: " Either email or password is wrong",
+        });
+    }
 
-    
-}
+    if (user) {
+        res.status(200).json({
+            success: true,
+            message: "User login successful",
+        });
+    }
+};
--- controller/userController.js
+++ controller/userController.js
@@ -1,74 +1,74 @@
 const res = require('express/lib/response');
 const User = require('../model/userModel');
 
-exports.getAllUser = async(req, res)=>{
+exports.getAllUser = async (req, res) => {
     const allUser = await User.find();
 
-    if(!allUser || allUser.length===0){
+    if (!allUser || allUser.length === 0) {
         return res.status(400).json({
-            success:false,
-            message:'No user registered yet'
+            success: false,
+            message: 'No user registered yet'
         })
     }
 
     res.status(200).json({
-        success:true,
-        numberOfUser:allUser.length,
-        data:{
-          
-            users:allUser
+        success: true,
+        numberOfUser: allUser.length,
+        data: {
+
+            users: allUser
         }
     })
 }
 
-exports.getSingleUser = async(req, res)=>{
+exports.getSingleUser = async (req, res) => {
 
     const findUser = await User.findById(req.params.id);
-    if(!findUser){
+    if (!findUser) {
         return res.status(400).json({
-            success:false,
-            message:'User doesnt exist'
+            success: false,
+            message: 'User doesnt exist'
         })
     }
 
     res.status(200).json({
-        success:true,
-        data:{
-            user:findUser
+        success: true,
+        data: {
+            user: findUser
         }
     })
 }
 
-exports.updateUserSelf = async (req,res)=>{
+exports.updateUserSelf = async (req, res) => {
     let userDetail = await User.findById(req.params.id);
 
-    if(!userDetail) return res.status(400).json({
-        success:false,
-        message:'User does not exist'
+    if (!userDetail) return res.status(400).json({
+        success: false,
+        message: 'User does not exist'
     })
 
-    userDetail = await User.findByIdAndUpdate(userDetail.id, req.body, {new:true, runValidators:true})
+    userDetail = await User.findByIdAndUpdate(userDetail.id, req.body, { new: true, runValidators: true })
 
     res.status(200).json({
-        success:true,
-        data:{
+        success: true,
+        data: {
             user: userDetail
         }
     })
 }
 
-exports.deleteUser = async (req,res)=>{
+exports.deleteUser = async (req, res) => {
 
-let removeUser = await User.findById(req.params.id);
-if(!removeUser) return res.status(400).json({
-    success:false,
-    message:'User does not exist'
-})
-removeUser= await User.findByIdAndRemove(removeUser.id);
+    let removeUser = await User.findById(req.params.id);
+    if (!removeUser) return res.status(400).json({
+        success: false,
+        message: 'User does not exist'
+    })
+    removeUser = await User.findByIdAndRemove(removeUser.id);
 
-res.status(203).json({
-    success:true,
-    message:'User has been removed'
-})
+    res.status(203).json({
+        success: true,
+        message: 'User has been removed'
+    })
 }
 
