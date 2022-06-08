b3d602a41417ee3f97fcd2e2e75155ede8dfb13d
Safar123 <azajsbd111@gmail.com>
2022-06-08 09:26:23 +0545

    Implementing file upload middelware

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,9 @@
 const express = require ('express');
-const { signUpUser, logInUser } = require('../controller/authController');
+const { signUpUser, logInUser, uploadUserPhoto } = require('../controller/authController');
 const { getAllUser, getSingleUser, updateUserSelf, deleteUser } = require('../controller/userController');
 
 const router = express.Router();
-
-router.route('/signUp').post(signUpUser);
+router.route('/signUp').post( uploadUserPhoto ,signUpUser);
 router.route('/').get(getAllUser);
 router.route('/:id').get(getSingleUser).patch(updateUserSelf).delete(deleteUser);
 router.route('/login').post(logInUser);
