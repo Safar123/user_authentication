164d34ba5b0f2d221d95b1770432b3c8da6403f5
Safar123 <azajsbd111@gmail.com>
2022-06-20 20:39:20 +0545

    protected route and route authorization

--- controller/authController.js
+++ controller/authController.js
@@ -1,79 +1,72 @@
+const {promisify} = require('util')
 const User = require("../model/userModel");
-const catchAsync = require('../utils/catchAsyncError');
-const GlobalError = require('../utils/globalError');
-const sharp = require('sharp')
-const multer = require('multer');
+const catchAsync = require("../utils/catchAsyncError");
+const GlobalError = require("../utils/globalError");
+const sharp = require("sharp");
+const multer = require("multer");
+const jwt = require("jsonwebtoken");
 
-//!Storing image on file on some folder structure using option like (destination)
-// const multerStorage = multer.diskStorage({
+const webToken = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRES_IN,
+    });
+};
 
-//     destination:(req, file, cb)=>{
-//         cb(null, 'public/img')
-//     },
-
-//     filename:(req, file, cb)=>{
-//         const ext = file.mimetype.split('/')[1];
-//         cb(null, `user-${Date.now()}.${ext}`)
-//     }
-// })
-
-const multerStorage = multer.memoryStorage()
+const multerStorage = multer.memoryStorage();
 
 const multerFilter = (req, file, cb) => {
-    if (file.mimetype.startsWith('image')) {
-        cb(null, true)
+    if (file.mimetype.startsWith("image")) {
+        cb(null, true);
     } else {
-        cb(new GlobalError('Invalid file type. Please upload image only', 400), false)
+        cb(
+            new GlobalError("Invalid file type. Please upload image only", 400),
+            false
+        );
     }
-}
+};
 
 const upload = multer({
     storage: multerStorage,
-    fileFilter: multerFilter
-})
+    fileFilter: multerFilter,
+});
 
-exports.resizeUserPhoto = async (req, res, next)=>{
+exports.uploadUserImage = upload.single("userImage");
+
+exports.resizeUserImage = (req, res, next) => {
     if (!req.file) return next();
+    req.file.filename = `NewUser-${Date.now()}-user.jpeg`;
 
-    req.file.filename= `user-${Date.now()}.jpeg`
     sharp(req.file.buffer)
-    .resize(500, 500)
-    .toFormat('jpeg')
-    .jpeg({ quality: 90 })
-    .toFile(`public/img/${req.file.filename}`)
-
+        .resize(500, 500)
+        .toFormat("jpeg")
+        .jpeg({ quality: 90 })
+        .toFile(`public/img/${req.file.filename}`);
     next();
-}
-
-
+};
 
 exports.signUpUser = catchAsync(async (req, res) => {
-
     if (req.body.password.match(/^[a-zA-Z][0-9]$/)) {
-        return res.status(400).json({
-            success: false,
-            message: "Password must contain both number and alphabet",
-        });
+        return next(
+            new GlobalError("Password must contain both number and letter", 401)
+        );
     }
-
     const newUser = await User.create({
-        username: req.body.username,
         email: req.body.email,
         password: req.body.password,
         confirmPassword: req.body.confirmPassword,
         userImage: req.file.filename,
-        role: req.body.role
+        role: req.body.role,
     });
 
+    const token = webToken(newUser._id);
     res.status(201).json({
         success: "true",
+        token,
         data: {
             user: newUser,
         },
     });
 });
-
-exports.uploadUserPhoto = upload.single('userImage');
 
 //!Login function
 
@@ -83,26 +76,65 @@ exports.logInUser = catchAsync(async (req, res, next) => {
 
     //!check we have both emil and password
     if (!email || !password) {
-        return res.status(400).json({
-            success: false,
-            message: "Both email and Password is required to login",
-        });
+        return next(new GlobalError("Email and Password is required field", 401));
     }
     //!then we need to check if email is registered or not
     const user = await User.findOne({ email }).select("+password");
 
     //!function to compre user input pwd with encrypted password in database
     if (!user || !(await user.checkPwdEncryption(password, user.password))) {
-        return res.status(400).json({
-            success: "fail",
-            message: " Either email or password is wrong",
-        });
+        return next(
+            new GlobalError(
+                "Either email or password didnt match user credentials",
+                401
+            )
+        );
     }
-
+    const token = webToken(user._id);
     if (user) {
         res.status(200).json({
             success: true,
-            message: "User login successful",
+            token,
         });
     }
 });
+
+exports.protectRoute = catchAsync(async (req, res, next) => {
+   let token;
+    if (
+        req.headers.authorization &&
+        req.headers.authorization.startsWith("Bearer")
+    ) {
+        token = req.headers.authorization.split(" ")[1];     
+    }
+    //checking if the signature is valid
+    const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+   
+    //check if user exist
+    const existUser = await User.findById(decoded.id);
+
+    if(!existUser){
+        return next(new GlobalError('User doesnt exist for the generated token. Please check credentials', 401))
+    }
+
+    //check if user has changed password after token was generated
+   if( existUser.checkIfPswdChanged(decoded.iat)){
+ return next(new GlobalError('User has changed password recently.', 401))
+   }
+
+   req.user = existUser;
+    next();
+});
+
+exports.authorizationRoutes = (...roles)=>{
+    return(req,res,next)=>{
+        if(!roles.includes(req.user.role)){
+
+            return next(new GlobalError(' You do not have access to complete this action', 403));
+        }
+
+        next();
+    }
+
+   
+}
