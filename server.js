89da56d5d0e8db0eb28bf88894058c67ddc65e53
Safar123 <azajsbd111@gmail.com>
2022-06-20 20:38:32 +0545

    unhandled rejection and uncaught exceptions

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const app = require("./app");
 const dotenv = require("dotenv");
 
+
+process.on('uncaughtException', err=>{
+  console.log('Uncaught exception error');
+  console.log(err.name, err.message);
+  process.exit(1);
+})
+
 dotenv.config({ path: "config/config.env" });
 
 //requiring database modules
@@ -9,6 +16,15 @@ dbConnection();
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
+const server=app.listen(port, () => {
   console.log(`App running on port ${port} in ${process.env.DEV_MODE} mode`);
 });
+
+process.on('unhandledRejection', err=>{
+  console.log('UNHANDLED REJECTION , Shutting down');
+  console.log(err.name, err.message);
+  server.close(()=>{
+    process.exit(1);
+  })
+});
+
