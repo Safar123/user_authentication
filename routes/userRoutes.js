e962a175e4b2899f562161fac2c456f0640977ea
Safar123 <azajsbd111@gmail.com>
2022-06-21 09:34:35 +0545

    formatting and indentation

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,27 @@
-const express = require ('express');
-const { signUpUser, logInUser,  resizeUserImage, uploadUserImage, protectRoute, authorizationRoutes } = require('../controller/authController');
-const { getAllUser, getSingleUser, updateUserSelf, deleteUser } = require('../controller/userController');
+const express = require("express");
+const {
+    signUpUser,
+    logInUser,
+    resizeUserImage,
+    uploadUserImage,
+    protectRoute,
+    authorizationRoutes,
+} = require("../controller/authController");
+const {
+    getAllUser,
+    getSingleUser,
+    updateUserSelf,
+    deleteUser,
+} = require("../controller/userController");
 
 const router = express.Router();
-router.route('/signUp').post(uploadUserImage, resizeUserImage, signUpUser);
-router.route('/').get(protectRoute, getAllUser);
-router.route('/:id').get(getSingleUser).patch(protectRoute,updateUserSelf).delete(protectRoute, authorizationRoutes('admin') , deleteUser);
-router.route('/login').post(logInUser);
+router.route("/signUp").post(uploadUserImage, resizeUserImage, signUpUser);
+router.route("/").get(protectRoute, getAllUser);
+router
+    .route("/:id")
+    .get(getSingleUser)
+    .patch(protectRoute, updateUserSelf)
+    .delete(protectRoute, authorizationRoutes("admin"), deleteUser);
+router.route("/login").post(logInUser);
 
 module.exports = router;
