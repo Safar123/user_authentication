c6c939ec4a4a623d0a7966178078f23495ff9c92
Safar123 <azajsbd111@gmail.com>
2022-06-21 22:09:22 +0545

    file to handle jwt sign and verify method

--- utils/jwtTokenHandler.js
+++ utils/jwtTokenHandler.js
@@ -1,6 +1,8 @@
 const GlobalError = require("./globalError");
+const jwt =require('jsonwebtoken');
+const { promisify } = require('util')
 
-const generateToken = (user)=>{
+exports.generateToken = (user)=>{
 
     const token = user.webToken(user._id);
     if(!token){
@@ -9,4 +11,12 @@ const generateToken = (user)=>{
     return token
 }
 
-module.exports = generateToken
+exports.verifyToken = async (vToken)=>{
+
+   const verified= await promisify(jwt.verify)(vToken, process.env.JWT_SECRET);
+   if(!verified){
+    return next(new GlobalError ('JWT token verification failed', 403))
+   }
+
+   return verified
+}
