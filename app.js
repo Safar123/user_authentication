5096e48e69061116f472a546ca72948e115e24cb
Safar123 <azajsbd111@gmail.com>
2022-06-27 23:28:34 +0545

    implementing all security measure

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 const express = require("express");
-const GlobalError = require('./utils/globalError')
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean')
+const rateLimiter = require('express-rate-limit');
+const GlobalError = require('./utils/globalError');
 
 const app = express();
+app.use(helmet());
 app.use(express.json()); //! body parse to parse user input into Json
 
+const limiter = rateLimiter({
+    max:100,
+    windowMs:60*60*1000,
+    message:'Too many request from this IP address. Please try again in one hour interval'
+})
+
+app.use('/api', limiter);
+app.use(mongoSanitize());
+app.use(xss());
 //!middelware 
 const errorHandler = require('./controller/errorController');
 
