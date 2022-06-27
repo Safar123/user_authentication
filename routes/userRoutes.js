2b896980b6232f8e03551141507b6eba32ba88ae
Safar123 <azajsbd111@gmail.com>
2022-06-27 08:32:16 +0545

    implementing protected route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,7 +7,9 @@ const {
     protectRoute,
     authorizationRoutes,
     forgetPassword,
-    resetPassword
+    resetPassword,
+    updatePassword,
+    updateMe,
 } = require("../controller/authController");
 const {
     getAllUser,
@@ -17,15 +19,25 @@ const {
 } = require("../controller/userController");
 
 const router = express.Router();
+
 router.route("/signUp").post(uploadUserImage, resizeUserImage, signUpUser);
-router.route("/").get(protectRoute, authorizationRoutes('admin', 'superadmin'), getAllUser);
-router
-    .route("/:id")
-    .get(protectRoute,getSingleUser)
+
+router .route("/")
+    .get(protectRoute, authorizationRoutes("admin", "superadmin"), getAllUser);
+
+router .route("/:id")
+    .get(protectRoute, getSingleUser)
     .patch(protectRoute, updateUserSelf)
-    .delete(protectRoute, authorizationRoutes("admin", 'superadmin'), deleteUser);
+    .delete(protectRoute, authorizationRoutes("admin", "superadmin"), deleteUser);
+
 router.route("/login").post(logInUser);
-router.post('/resetPassword/:token', resetPassword)
-router.post('/forgetPassword', forgetPassword)
+
+router.patch("/resetPassword/:token", resetPassword);
+
+router.post("/forgetPassword", forgetPassword);
+
+router.patch("/login/updatePassword", protectRoute, updatePassword);
+
+router.patch("/updateMe", protectRoute, updateMe);
 
 module.exports = router;
