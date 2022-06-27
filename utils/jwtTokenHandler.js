c85e00065ea58003781bd2b311feaf22202839d9
Safar123 <azajsbd111@gmail.com>
2022-06-27 23:12:15 +0545

    refactor handler with statuscode response and cookie

--- utils/jwtTokenHandler.js
+++ utils/jwtTokenHandler.js
@@ -2,13 +2,32 @@ const GlobalError = require("./globalError");
 const jwt =require('jsonwebtoken');
 const { promisify } = require('util')
 
-exports.generateToken = (user)=>{
+exports.generateToken = (user,statusCode, res)=>{
 
     const token = user.webToken(user._id);
+
+    const cookieOption ={
+        expire:new Date(Date.now())+ process.env.JWT_COOKIE_EXPIRE*24*60*60*1000,
+        httpOnly:true
+    }
+
+    if(process.env.DEV_MODE==='production') cookieOption.secure=true;
+
+    user.password =undefined;
+
+    res.cookie('jwt', token, cookieOption)
+
+
     if(!token){
         return next (new GlobalError('Generating token failed', 400))
     }
-    return token
+   res.status(statusCode).json({
+    status:'success',
+    token,
+    data:{
+        user
+    }
+   })
 }
 
 exports.verifyToken = async (vToken)=>{
